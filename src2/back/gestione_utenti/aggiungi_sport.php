b1988ce901b79ab6055c7ce2a753a6963d2adc66
Tr1bal06 <tribal06magrini@gmail.com>
2025-09-12 17:09:11 +0200

    inizata logica per accettare le richieste degli sport

--- src2/back/gestione_utenti/aggiungi_sport.php
+++ src2/back/gestione_utenti/aggiungi_sport.php
@@ -1,5 +1,5 @@
 <?php
-
+    
     include '../connessione.php';
     include '../function.php';
     if (session_status() == PHP_SESSION_NONE) {
@@ -17,18 +17,50 @@
         $sport =  htmlentities($_POST['sport']);
         $reach = htmlentities($_POST['source']);
         $codiciCariche = $_SESSION['caricheCodici'];
-        
-        if(empty($path)){
-            error('../../front/404.php', 'Inserimento sport fallito!');
-        }
-        if($reach == 'atleta') {
-            $codAtleta = $_SESSION['caricheCodici']['Atleta'];
-            $stmt1 = $conn->prepare("INSERT INTO ISCRIZIONE(CodiceAtleta, NomeSport) VALUES (?, ?)");
-            $stmt1->bind_param("is",$codAtleta , $sport); 
-        } else {
-            $codAllenatore = $_SESSION['caricheCodici']['Allenatore'];
-            $stmt1 = $conn->prepare("INSERT INTO INSEGNA(CodiceAllenatore, NomeSport) VALUES (?,?)");
-            $stmt1->bind_param("is",$codAllenatore , $sport); 
+        $path = '../../front/persone/persone.php';
+        $cod = htmlspecialchars($_POST['codice']);
+
+        die();
+        if($reach == 'Atleta') {
+            if($_POST['tipologia']=='Eliminazione'){
+                //TODO bisogna fare la logica per eliminare lo sport
+
+
+            }else if($_POST['tipologia']=='Inserimento'){
+                
+                $stmt1 = $conn->prepare("INSERT INTO ISCRIZIONE(CodiceAtleta, NomeSport) VALUES (?, ?)");
+                $stmt1->bind_param("is",$codAtleta , $sport);
+
+                //TODO bisogna cambiare lo stato della richiesta accettata TRANSAZIONE
+                $sql = "UPDATE RICHIESTE_ATL SET Stato='Confermato' WHERE CodicePersona=? AND Sport=?";
+                $stmt2 = $conn->prepare($sql);
+                $stmt2->bind_param("is", $cod, $sport);
+                $stmt2->execute();
+
+                if($stmt2->affected_rows === 0) {
+                    error($path, 'Errore nel cambiare stato della richiesta!');
+                }
+
+            }else{
+                error($path, 'Errore nel inserimento sport!');
+            }
+                
+            
+        } else if($reach == 'Allenatore') {
+            if($_POST['tipologia']=='Eliminazione'){
+                //TODO bisogna fare la logica per eliminare lo sport
+            }else if($_POST['tipologia']=='Inserimento'){
+                $stmt1 = $conn->prepare("INSERT INTO INSEGNA(CodiceAllenatore, NomeSport) VALUES (?,?)");
+                $stmt1->bind_param("is",$cod, $sport); 
+                
+                //TODO bisogna cambiare lo stato della richiesta accettata TRANSAZIONE
+
+            }else{
+                error($path, 'Errore nel inserimento sport!');
+            }
+            
+        }else {
+            error($path, 'Inserimento sport fallito!');
         }
         $stmt1->execute();
         if($stmt1->affected_rows === 0) {
