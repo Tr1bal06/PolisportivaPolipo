08d40f5276bb25ff04dd65901413244522009614
Asakura-06 <soulr3aper06@gmail.com>
2025-09-17 15:27:11 +0200

    sistemato la prenotazione iniziato il partecipa torneo

--- src2/back/tornei/PartecipaTornei.php
+++ src2/back/tornei/PartecipaTornei.php
@@ -1,46 +1,66 @@
 <?php
+
+    /**
+     * File: PartecipaTorneo.php
+     * Auth: Jin
+     * Desc: Questo file serve per l'iscrizione delle squadre ad un torneo , 
+     *       a controllare se il torneo è pieno per far partire il matchmatching , 
+     *       e controllare se è tempo di passare al prossimo round 
+     */
     include '../connessione.php';
     include '../function.php';
     if (session_status() == PHP_SESSION_NONE) {
-    // Avvia la sessione
-    session_start();
-}
+        // Avvia la sessione
+        session_start();
+    }
     //controllo i permessi
     
-    $permessi  =['user', 'admin'];
+    $permessi  =['Allenatore', 'admin'];
     
     if(!controllo($_SESSION['ruolo'], $permessi)){
         error('../../front/404.php', 'Accesso negato');
     }
 
+    $conn->begin_transaction();
+
     try{
         if($_SERVER['REQUEST_METHOD']=='POST'){
         
-            $codTorneo = $_POST['CodiceTorneo'];
-            $Anno = $_POST['Anno'];
-            $CF = $_SESSION['cf'];
+            $codTorneo = htmlentities($_POST['CodiceTorneo']);
+            $Anno = htmlentities($_POST['Anno']);
+            $nomeSquadra = htmlentities($_POST['Squadra']);
         }
+
+        $max = $conn->prepare("SELECT MaxSquadre
+                                 FROM EDIZIONE_TORNEO
+                                 WHERE CodiceTorneo = ? AND Anno= ?");
+        $max->bind_param("is",$codTorneo,$Anno);
+        $max->execute();
+        $result = $max->get_result();
+        $row = $result->fetch_assoc();
+
+        $check = $conn->prepare("SELECT NomeSquadra
+                                 FROM ISCRIZIONI_TORNEO
+                                 WHERE EdizioneTorneo = ? AND AnnoTorneo = ?");
+        $check->bind_param("is",$codTorneo,$Anno);
+        $check->execute();
+        $result = $check->get_result();
+        $row = $result->fetch_assoc();
+
         
-        //utilizzo una query preparata per evitare sql injection
-        $stmt=$conn->prepare("INSERT INTO PARTECIPAZIONE(CodiceTorneo, Anno, CFPartecipante)
-                              VALUES (?,?,?)");
-        $stmt->bind_param("iss",$codTorneo,$Anno,$CF);
-    
-        //controllo il risultato
-        if (($stmt->execute())) {
-            success('../../front/tornei/tornei.php', 'Iscrizione al torneo avvenuta con successo!');
-        } else {
-            
-            error('../../front/tornei/tornei.php', 'Iscrizione al torneo fallita!');
-        }
         
-        $stmt->close();
         $conn->commit();
     }catch(Exception $e){
         $conn->rollback();
-        error('../../front/tornei/tornei.php', 'Iscrizione al torneo fallita!');
+        $default = "";
+
+        $codiciGestiti = [];
+
+        if (in_array($e->getCode(), $codiciGestiti, true)) {
+            $default = $e->getMessage();
+        }
+
+        error('../../front/tornei/tornei.php', $default);
     }
-    //salvo le variabili di sessione
-  
-    //made by Tha_Losco
+    //success('../../front/tornei/tornei.php', 'Iscrizione al torneo avvenuta con successo!'); ??
 ?>
